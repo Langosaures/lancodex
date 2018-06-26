a0cefa3c553c258a281c464bf7f90291c3f6b2e3
Ludivine Loignon <acilulu@gmail.com>
2018-06-26 14:24:32 +0200

    bdd avec questions + rep

--- bdgestionprojets.sql
+++ bdgestionprojets.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 26 juin 2018 à 09:35
+-- Généré le :  mar. 26 juin 2018 à 14:21
 -- Version du serveur :  10.1.31-MariaDB
 -- Version de PHP :  7.2.3
 
@@ -96,8 +96,35 @@ CREATE TABLE `question` (
   `id` int(11) NOT NULL,
   `libelle` varchar(255) NOT NULL,
   `idTechnologie` int(11) NOT NULL,
-  `idBonneReponse` int(11) NOT NULL
+  `idBonneReponse` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `question`
+--
+
+INSERT INTO `question` (`id`, `libelle`, `idTechnologie`, `idBonneReponse`) VALUES
+(1, 'Que veut dire HTML ?', 1, 1),
+(2, 'Quel est le logo correspondant ?', 1, 9),
+(3, 'A quoi sert ce langage ?', 1, 5),
+(4, 'Que veut dire CSS ?', 2, 12),
+(5, 'Quel est le logo correspondant ?', 2, 14),
+(6, 'A quoi sert le CSS ?', 2, 15),
+(7, 'Que veut dire JAVA ?', 4, 19),
+(8, 'Quel est le logo correspondant ?', 4, 23),
+(9, 'A quoi sert ce langage ?', 4, 25),
+(10, 'Que veut dire PHP ?', 3, 30),
+(11, 'Quel est le logo correspondant ?', 3, 32),
+(12, 'A quoi sert ce langage ?', 3, 36),
+(13, 'Quel est le bon nom de ce langage ?', 5, 50),
+(14, 'Quel est le logo correspondant ?', 5, 53),
+(15, 'A quoi sert Angular ?', 5, 54),
+(16, 'Que veut dire SQL ?', 6, 61),
+(17, 'Quel est le logo correspondant ?', 6, 62),
+(18, 'A quoi sert ce langage ?', 6, 65),
+(19, 'A quoi correspond Python ?', 7, 69),
+(20, 'Quel est le logo correspondant ?', 7, 71),
+(21, 'A quoi sert ce langage ?', 7, 75);
 
 -- --------------------------------------------------------
 
@@ -107,9 +134,78 @@ CREATE TABLE `question` (
 
 CREATE TABLE `reponse` (
   `id` int(11) NOT NULL,
-  `libelle` int(11) NOT NULL,
+  `libelle` varchar(255) NOT NULL,
   `idQuestion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reponse`
+--
+
+INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
+(1, 'HyperText Markup Language', 1),
+(2, 'How To Make Lasagna', 1),
+(3, 'Holistic Technical Method Library', 1),
+(4, 'Hyper Text Making Language', 1),
+(5, 'A mettre en forme des pages web', 3),
+(6, 'A ajouter du style à une page web', 3),
+(7, 'A créer des jeux vidéos', 3),
+(8, 'A executer du code Python', 3),
+(9, 'html.png', 2),
+(10, 'Computer Style Sheets', 4),
+(11, 'Choucroute et Saucisse d\'Alsace', 4),
+(12, 'Cascading Style Sheets', 4),
+(13, 'Create Simple Samples', 4),
+(14, 'css.png', 5),
+(15, 'A ajouter du style aux pages web', 6),
+(16, 'A insérer du contenu dans une page internet', 6),
+(17, 'A réaliser des pages dynamiques', 6),
+(18, 'A faire de jolis dégradés de couleurs', 6),
+(19, 'En argot américain, il signifie café', 7),
+(20, 'Une île en Indonésie', 7),
+(21, 'Une danse', 7),
+(22, 'Un ordinateur', 7),
+(23, 'java.png', 8),
+(24, 'C\'est un langage de programmation utilisé pour créer des pages web', 9),
+(25, 'C\'est un langage de programmation orienté objet', 9),
+(26, 'Il est utilisé pour programmer des cafetières', 9),
+(27, 'Il est utilisé pour faire le design des pages Web', 9),
+(28, 'Page Helper Process', 10),
+(29, 'Personnal HyperText Processor', 10),
+(30, 'PHP HyperText Preprocessor', 10),
+(31, 'Private Home Page', 10),
+(32, 'php.png', 11),
+(33, 'A traiter des informations côté client', 12),
+(34, 'A faire fonctionner Facebook', 12),
+(35, 'A ajouter du style sur une page web', 12),
+(36, 'Généralement il sert à traiter des informations de formulaires ou de base de données côté serveur', 12),
+(49, 'Engulaire', 13),
+(50, 'Angular', 13),
+(51, 'Angulair', 13),
+(52, 'Langular', 13),
+(53, 'angularjs.png', 14),
+(54, 'Créer facilement des applications web avec une navigation côté client', 15),
+(55, 'Générer des applications lourdes', 15),
+(56, 'Traiter des informations venant de la base de données', 15),
+(57, 'Ne pas avoir à utiliser la logique MVC', 15),
+(58, 'Simple Quite Language', 16),
+(59, 'Structured Question Language', 16),
+(60, 'Strong Question Language', 16),
+(61, 'Structured Query Language', 16),
+(62, 'sql.png', 17),
+(63, 'A faciliter la navigation que les applications web', 18),
+(64, 'A voler les données des utilisateurs de ce site', 18),
+(65, 'Exploiter des bases de données', 18),
+(66, 'Créer des pages web', 18),
+(67, 'La constellation du dragon', 19),
+(68, 'Un monstrueux serpent', 19),
+(69, 'Un langage de programmation interprété', 19),
+(70, 'L\'idole des serpentard', 19),
+(71, 'python.png', 20),
+(72, 'Apprendre à parler le fourchelangue', 21),
+(73, 'Communiquer avec des bases de données', 21),
+(74, 'Créer des applications web', 21),
+(75, 'Créer des programmes simples ou des progiciels', 21);
 
 -- --------------------------------------------------------
 
@@ -216,13 +312,13 @@ ALTER TABLE `indice`
 -- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT pour la table `reponse`
 --
 ALTER TABLE `reponse`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT pour la table `technologie`
