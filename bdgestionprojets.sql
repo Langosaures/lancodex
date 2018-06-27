d444b5d1dd7d28b55b94adc3c2386161399959ee
Baboulinux <matteo.dervillers@epsi.fr>
2018-06-27 09:29:55 +0200

    AddNewBdd

--- bdgestionprojets.sql
+++ bdgestionprojets.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mar. 26 juin 2018 à 14:21
--- Version du serveur :  10.1.31-MariaDB
--- Version de PHP :  7.2.3
+-- Host: 127.0.0.1
+-- Generation Time: Jun 27, 2018 at 09:27 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `bdgestionprojets`
+-- Database: `bdgestionprojets`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `indice`
+-- Table structure for table `indice`
 --
 
 CREATE TABLE `indice` (
@@ -35,24 +35,24 @@ CREATE TABLE `indice` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `indice`
+-- Dumping data for table `indice`
 --
 
 INSERT INTO `indice` (`id`, `libelle`, `idTechno`) VALUES
 (1, 'Il y a un logo hexagonal s\'appelant Angular…', 5),
 (2, 'Le rouge c\'est dynamique, comme Angular !', 5),
 (3, 'Angular rime avec rouge non ?', 5),
-(4, 'La couleur du Css est la même que celle du dauphin..', 2),
-(5, 'Le Css est compagnon du Html dans la forme !', 2),
+(4, 'La couleur du CSS est la même que celle du dauphin..', 2),
+(5, 'Le CSS est compagnon du Html dans la forme !', 2),
 (6, 'Le compagnon du Html est un superbe designer !', 2),
 (7, 'Je suis orange et je crée la structure des pages..', 1),
 (8, 'Le HTML est un compagnon du Css', 1),
 (9, 'Le HTML est de forme pentagonal', 1),
-(10, 'J\'aime appeler le java Polux!', 4),
+(10, 'J\'aime appeler le JAVA Polux!', 4),
 (11, 'Polux aime le café..', 4),
 (12, 'Rouge est le café..', 4),
-(13, 'Le Php est bleu !', 3),
-(14, 'Php est le tatouage de l\'éléphant', 3),
+(13, 'Le PHP est bleu !', 3),
+(14, 'PHP est le tatouage de l\'éléphant', 3),
 (15, 'L\'éléphant code des pages Webs dynamiques !', 3),
 (16, 'Je porte le nom d\'un serpent', 7),
 (17, 'J\'aime les pythons !', 7),
@@ -71,7 +71,7 @@ INSERT INTO `indice` (`id`, `libelle`, `idTechno`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `maitrise`
+-- Table structure for table `maitrise`
 --
 
 CREATE TABLE `maitrise` (
@@ -80,7 +80,7 @@ CREATE TABLE `maitrise` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `maitrise`
+-- Dumping data for table `maitrise`
 --
 
 INSERT INTO `maitrise` (`idUtilisateur`, `idTechnologie`) VALUES
@@ -89,7 +89,7 @@ INSERT INTO `maitrise` (`idUtilisateur`, `idTechnologie`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `question`
+-- Table structure for table `question`
 --
 
 CREATE TABLE `question` (
@@ -100,7 +100,7 @@ CREATE TABLE `question` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `question`
+-- Dumping data for table `question`
 --
 
 INSERT INTO `question` (`id`, `libelle`, `idTechnologie`, `idBonneReponse`) VALUES
@@ -129,7 +129,7 @@ INSERT INTO `question` (`id`, `libelle`, `idTechnologie`, `idBonneReponse`) VALU
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reponse`
+-- Table structure for table `reponse`
 --
 
 CREATE TABLE `reponse` (
@@ -139,7 +139,7 @@ CREATE TABLE `reponse` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `reponse`
+-- Dumping data for table `reponse`
 --
 
 INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
@@ -210,7 +210,7 @@ INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `technologie`
+-- Table structure for table `technologie`
 --
 
 CREATE TABLE `technologie` (
@@ -221,7 +221,7 @@ CREATE TABLE `technologie` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `technologie`
+-- Dumping data for table `technologie`
 --
 
 INSERT INTO `technologie` (`id`, `libelle`, `description`, `logo`) VALUES
@@ -230,13 +230,13 @@ INSERT INTO `technologie` (`id`, `libelle`, `description`, `logo`) VALUES
 (3, 'php', 'L\'Hypertext Processor est un langage de programmation libre permettant de créer des pages Web dynamiques et fonctionnant avec la plupart des langages interprétés ( javascript, html…)', 'php.png'),
 (4, 'java', 'Le Java est un langage compilé ayant la particularité de créer des applications facilement portables sur divers systèmes d\'exploitation comme Unix, Mac OS, Windows', 'java.png'),
 (5, 'angular', 'Angular est un framework du langage Javascript. Celui-ci rend le HTML dynamique pour permettre au développeur de créer ses propres balises et attributs. Ce framework pousse le développeur à une création structurée et a pour but d\'apporter un aspect applicatif au front-office.', 'angularjs.png'),
-(6, 'sql', 'Le Structured Query Language est un langage informatique utilisé pour la gestion de base de données. Celui-ci envoie des requêtes à la BDD pour intéragir avec ses informations.', 'java.png'),
+(6, 'sql', 'Le Structured Query Language est un langage informatique utilisé pour la gestion de base de données. Celui-ci envoie des requêtes à la BDD pour intéragir avec ses informations.', 'sql.png'),
 (7, 'python', 'Le python est un langage de programmation objet, multi-paradigme et multiplateforme favorisant la programmation structurée, fonctionnelle et objet. Il convient à tous types de développeurs.', 'python.png');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
@@ -247,32 +247,32 @@ CREATE TABLE `utilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `utilisateur`
+-- Dumping data for table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`email`, `mdp`, `nom`, `prenom`) VALUES
 ('admin@simpleduc.org', '$2y$10$q8.AZxrDgmrHediCOUPvpOyDJJuOP70041CHa8NYq9QlhTsYiYXjG', 'admin', 'admin');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `indice`
+-- Indexes for table `indice`
 --
 ALTER TABLE `indice`
   ADD PRIMARY KEY (`id`),
   ADD KEY `techno` (`idTechno`);
 
 --
--- Index pour la table `maitrise`
+-- Indexes for table `maitrise`
 --
 ALTER TABLE `maitrise`
   ADD PRIMARY KEY (`idUtilisateur`,`idTechnologie`) USING BTREE,
   ADD KEY `maitrise2` (`idTechnologie`);
 
 --
--- Index pour la table `question`
+-- Indexes for table `question`
 --
 ALTER TABLE `question`
   ADD PRIMARY KEY (`id`),
@@ -280,78 +280,78 @@ ALTER TABLE `question`
   ADD KEY `idBonneReponse` (`idBonneReponse`);
 
 --
--- Index pour la table `reponse`
+-- Indexes for table `reponse`
 --
 ALTER TABLE `reponse`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idQuestion` (`idQuestion`);
 
 --
--- Index pour la table `technologie`
+-- Indexes for table `technologie`
 --
 ALTER TABLE `technologie`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `utilisateur`
+-- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`email`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `indice`
+-- AUTO_INCREMENT for table `indice`
 --
 ALTER TABLE `indice`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT pour la table `question`
+-- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
--- AUTO_INCREMENT pour la table `reponse`
+-- AUTO_INCREMENT for table `reponse`
 --
 ALTER TABLE `reponse`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
--- AUTO_INCREMENT pour la table `technologie`
+-- AUTO_INCREMENT for table `technologie`
 --
 ALTER TABLE `technologie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `indice`
+-- Constraints for table `indice`
 --
 ALTER TABLE `indice`
   ADD CONSTRAINT `techno` FOREIGN KEY (`idTechno`) REFERENCES `technologie` (`id`);
 
 --
--- Contraintes pour la table `maitrise`
+-- Constraints for table `maitrise`
 --
 ALTER TABLE `maitrise`
   ADD CONSTRAINT `maitrise2` FOREIGN KEY (`idTechnologie`) REFERENCES `technologie` (`id`),
   ADD CONSTRAINT `maitriser` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`email`);
 
 --
--- Contraintes pour la table `question`
+-- Constraints for table `question`
 --
 ALTER TABLE `question`
   ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`idTechnologie`) REFERENCES `technologie` (`id`),
   ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`idBonneReponse`) REFERENCES `reponse` (`id`);
 
 --
--- Contraintes pour la table `reponse`
+-- Constraints for table `reponse`
 --
 ALTER TABLE `reponse`
   ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`);
