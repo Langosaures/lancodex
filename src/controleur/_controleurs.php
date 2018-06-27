9e0d590196099d747928aa3000776110ba214edf
Baboulinux <matteo.dervillers@epsi.fr>
2018-06-27 14:29:39 +0200

    addQuizzWithoutTest+bdd

--- src/controleur/_controleurs.php
+++ src/controleur/_controleurs.php
@@ -2,7 +2,6 @@
   require_once 'controleur_index.php';
   require_once 'controleur_utilisateur.php';
   require_once 'controleur_techno.php';
-  require_once 'controleur_QR.php';
   require_once 'controleur_lancodex.php';
 ?>
 
--- src/modele/class_QR.php
+++ src/modele/class_QR.php
@@ -9,7 +9,7 @@ class QR{
     public function __construct($db){
         $this->db = $db;  
         // requête permettant de sélectionner une question avec sa bonne réponse et son logo
-        $this->select = $db->prepare("select q.id, q.libelle as question, t.logo, t.libelle as techno, r.libelle as reponse  from question q, technologie t, reponse r  where q.idTechnologie = t.id and q.idBonneReponse = r.id and id = :id");
+        $this->select = $db->prepare("select q.id, q.libelle, t.logo, t.libelle, r.libelle from question q, technologie t, reponse r  where q.idTechnologie = t.id and q.idBonneReponse = r.id and q.id = :id");
         //requête permettant de récupérer les mauvaises réponses par question
         $this->selectFausseRep = $db->prepare("select r.libelle from reponse r, question q where q.id = r.idQuestion and r.id not in (select q.idBonneReponse from question q) and idQuestion = :id");
 
@@ -30,7 +30,7 @@ class QR{
         if ($this->selectFausseRep->errorCode()!=0){
              print_r($this->selectFausseRep->errorInfo());  
         }
-        return $this->selectFausseRep->fetch();
+        return $this->selectFausseRep->fetchAll();
     }
 }
 
