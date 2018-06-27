8bf7217da3aa8a3def4baf2b2c670e4d97b51a70
Ludivine Loignon <acilulu@gmail.com>
2018-06-27 14:56:59 +0200

    bdd en utf8

--- bdgestionprojets.sql
+++ bdgestionprojets.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 27, 2018 at 09:47 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 27 juin 2018 à 14:56
+-- Version du serveur :  10.1.31-MariaDB
+-- Version de PHP :  7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,59 +19,59 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bdgestionprojets`
+-- Base de données :  `bdgestionprojets`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `indice`
+-- Structure de la table `indice`
 --
 
 CREATE TABLE `indice` (
   `id` int(11) NOT NULL,
-  `libelle` varchar(255) NOT NULL,
+  `libelle` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `idTechno` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `indice`
+-- Déchargement des données de la table `indice`
 --
 
 INSERT INTO `indice` (`id`, `libelle`, `idTechno`) VALUES
-(1, 'Il y a un logo hexagonal s\'appelant Angular…', 5),
+(1, 'Il y a un logo hexagonal s\'appelant Angular . . .', 5),
 (2, 'Le rouge c\'est dynamique, comme Angular !', 5),
 (3, 'Angular rime avec rouge non ?', 5),
-(4, 'La couleur du CSS est la même que celle du dauphin..', 2),
-(5, 'Le CSS est compagnon du Html dans la forme !', 2),
+(4, 'La couleur du Css est la même que celle du dauphin . . .', 2),
+(5, 'Le CSS est compagnon du HTML dans la forme !', 2),
 (6, 'Le compagnon du Html est un superbe designer !', 2),
-(7, 'Je suis orange et je crée la structure des pages..', 1),
-(8, 'Le HTML est un compagnon du Css', 1),
+(7, 'Je suis orange et je crée la structure des pages . . .', 1),
+(8, 'Le HTML est un compagnon du Css !', 1),
 (9, 'Le HTML est de forme pentagonal', 1),
-(10, 'J\'aime appeler le JAVA Polux!', 4),
-(11, 'Polux aime le café..', 4),
-(12, 'Rouge est le café..', 4),
-(13, 'Le PHP est bleu !', 3),
-(14, 'PHP est le tatouage de l\'éléphant', 3),
-(15, 'L\'éléphant code des pages Webs dynamiques !', 3),
-(16, 'Je porte le nom d\'un serpent', 7),
+(10, 'J\'aime appeler le java Polux !', 4),
+(11, 'Polux aime le café . . .', 4),
+(12, 'Rouge est le café . . .', 4),
+(13, 'Le Php est bleu !', 3),
+(14, 'Php est le tatouage de l\'éléphant !', 3),
+(15, 'L\'éléphant code des pages Web dynamiques !', 3),
+(16, 'Je porte le nom d\'un serpent . . .', 7),
 (17, 'J\'aime les pythons !', 7),
 (18, 'Avez-vous déjà vu une croix gammée bleu et jaune? Moi si !', 7),
-(19, 'Le SQL est d\'une forme cylindrique', 6),
+(19, 'Le SQL est d\'une forme cylindrique . . .', 6),
 (20, 'Un cylindre est une représentation de disques de données', 6),
 (21, 'J\'aime les cylindre bleu, et vert aussi !', 6),
 (22, 'Je m\'appelle Gerafe, tu veux être mon ami ?', NULL),
 (23, 'J\'aime le pain à la confiture !', NULL),
 (24, 'Quel est le dinosaure le plus sesuel ? Le Tripotanus !', NULL),
 (25, 'Qu\'est-ce qu\'un dinosaure qui joue à la loterie ? Un Tirageosaure !', NULL),
-(26, 'Comment sais-tu qu\'un dinosaure est caché sous ton lit? Tu as le nez collé au plafond', NULL),
-(27, 'Qu’est-ce qu’une maman dinosaure raconte a son bébé dinosaure? Une préhistoire! ', NULL),
+(26, 'Comment sais-tu qu\'un dinosaure est caché sous ton lit? Tu as le nez collé au plafond !', NULL),
+(27, 'Qu\'est-ce qu\'une maman dinosaure raconte a son bébé dinosaure? Une préhistoire! ', NULL),
 (28, 'Pourquoi les dinosaures ne peuvent pas taper des mains ? Parce qu\'ils sont mort idiot !', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `maitrise`
+-- Structure de la table `maitrise`
 --
 
 CREATE TABLE `maitrise` (
@@ -80,7 +80,7 @@ CREATE TABLE `maitrise` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `maitrise`
+-- Déchargement des données de la table `maitrise`
 --
 
 INSERT INTO `maitrise` (`idUtilisateur`, `idTechnologie`) VALUES
@@ -89,7 +89,7 @@ INSERT INTO `maitrise` (`idUtilisateur`, `idTechnologie`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `question`
+-- Structure de la table `question`
 --
 
 CREATE TABLE `question` (
@@ -100,7 +100,7 @@ CREATE TABLE `question` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `question`
+-- Déchargement des données de la table `question`
 --
 
 INSERT INTO `question` (`id`, `libelle`, `idTechnologie`, `idBonneReponse`) VALUES
@@ -129,17 +129,17 @@ INSERT INTO `question` (`id`, `libelle`, `idTechnologie`, `idBonneReponse`) VALU
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reponse`
+-- Structure de la table `reponse`
 --
 
 CREATE TABLE `reponse` (
   `id` int(11) NOT NULL,
-  `libelle` varchar(255) NOT NULL,
+  `libelle` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
   `idQuestion` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `reponse`
+-- Déchargement des données de la table `reponse`
 --
 
 INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
@@ -186,7 +186,7 @@ INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
 (53, 'angularjs.png', 14),
 (54, 'Créer facilement des applications web avec une navigation côté client', 15),
 (55, 'Générer des applications lourdes', 15),
-(56, 'Traiter des informations venant de la base de données', 15),
+(56, 'Traiter des informations venant de la base de donn?es', 15),
 (57, 'Ne pas avoir à utiliser la logique MVC', 15),
 (58, 'Simple Quite Language', 16),
 (59, 'Structured Question Language', 16),
@@ -195,14 +195,14 @@ INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
 (62, 'sql.png', 17),
 (63, 'A faciliter la navigation que les applications web', 18),
 (64, 'A voler les données des utilisateurs de ce site', 18),
-(65, 'Exploiter des bases de données', 18),
+(65, 'Exploiter des bases de donn?es', 18),
 (66, 'Créer des pages web', 18),
 (67, 'La constellation du dragon', 19),
 (68, 'Un monstrueux serpent', 19),
 (69, 'Un langage de programmation interprété', 19),
 (70, 'L\'idole des serpentard', 19),
 (71, 'python.png', 20),
-(72, 'Apprendre à parler le fourchelangue', 21),
+(72, 'Apprendre à parler le fourchelang', 21),
 (73, 'Communiquer avec des bases de données', 21),
 (74, 'Créer des applications web', 21),
 (75, 'Créer des programmes simples ou des progiciels', 21);
@@ -210,33 +210,33 @@ INSERT INTO `reponse` (`id`, `libelle`, `idQuestion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `technologie`
+-- Structure de la table `technologie`
 --
 
 CREATE TABLE `technologie` (
   `id` int(11) NOT NULL,
   `libelle` varchar(255) NOT NULL,
   `description` varchar(300) NOT NULL,
-  `logo` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `logo` varchar(255) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `technologie`
+-- Déchargement des données de la table `technologie`
 --
 
 INSERT INTO `technologie` (`id`, `libelle`, `description`, `logo`) VALUES
 (1, 'HTML', 'Le HyperText Markup Language  est un langage permettant de créer le squelette d\'une page web à l\'aide de balises. Celui-ci nous permet d\'écrire de l\'hypertext, d\'où son nom.', 'html.png'),
 (2, 'CSS', 'Le Cascading Style Sheets est un langage complémentaire du HTML car il permet de gérer la mise en page d\'un site, son design.', 'css.png'),
-(3, 'PHP', 'L\'Hypertext Processor est un langage de programmation libre permettant de créer des pages Web dynamiques et fonctionnant avec la plupart des langages interprétés ( javascript, html…)', 'php.png'),
+(3, 'PHP', 'L\'Hypertext PreProcessor est un langage de programmation libre permettant de créer des pages Web dynamiques et fonctionnant avec la plupart des langages interprétés ( Javascript, HTML . . .)', 'php.png'),
 (4, 'Java', 'Le Java est un langage compilé ayant la particularité de créer des applications facilement portables sur divers systèmes d\'exploitation comme Unix, Mac OS, Windows', 'java.png'),
 (5, 'Angular', 'Angular est un framework du langage Javascript. Celui-ci rend le HTML dynamique pour permettre au développeur de créer ses propres balises et attributs. Ce framework pousse le développeur à une création structurée et a pour but d\'apporter un aspect applicatif au front-office.', 'angularjs.png'),
-(6, 'SQL', 'Le Structured Query Language est un langage informatique utilisé pour la gestion de base de données. Celui-ci envoie des requêtes à la BDD pour intéragir avec ses informations.', 'sql.png'),
+(6, 'SQL', 'Le Structured Query Language est un langage informatique utilisé pour la gestion de base de données. Celui-ci envoie des requêtes à la base de données pour interagir avec ses informations.', 'sql.png'),
 (7, 'Python', 'Le Python est un langage de programmation objet, multi-paradigme et multiplateforme favorisant la programmation structurée, fonctionnelle et objet. Il convient à tous types de développeurs.', 'python.png');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateur`
+-- Structure de la table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
@@ -247,32 +247,35 @@ CREATE TABLE `utilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `utilisateur`
+-- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`email`, `mdp`, `nom`, `prenom`) VALUES
-('admin@simpleduc.org', '$2y$10$q8.AZxrDgmrHediCOUPvpOyDJJuOP70041CHa8NYq9QlhTsYiYXjG', 'admin', 'admin');
+('admin@simpleduc.org', '$2y$10$q8.AZxrDgmrHediCOUPvpOyDJJuOP70041CHa8NYq9QlhTsYiYXjG', 'admin', 'admin'),
+('test2@test.org', '$2y$10$SJ0fT47wQas0sfeV7JStBetaKqrZoQHqbVr5FQ0FxLnUYzwwlXzlu', '2', 'test'),
+('test3@test3.org', '$2y$10$oEZbd01apifHuVhudHj0QuGTX9iseBQAtKDHuwdqum7cIZV7yfjY2', '2', 'Test'),
+('test@test.org', '$2y$10$YlzNu3D.hfuslPYrijBzhexYp/koEJrYhLhSqlekO3n4JC7DSFeBG', '2', 'test');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `indice`
+-- Index pour la table `indice`
 --
 ALTER TABLE `indice`
   ADD PRIMARY KEY (`id`),
   ADD KEY `techno` (`idTechno`);
 
 --
--- Indexes for table `maitrise`
+-- Index pour la table `maitrise`
 --
 ALTER TABLE `maitrise`
   ADD PRIMARY KEY (`idUtilisateur`,`idTechnologie`) USING BTREE,
   ADD KEY `maitrise2` (`idTechnologie`);
 
 --
--- Indexes for table `question`
+-- Index pour la table `question`
 --
 ALTER TABLE `question`
   ADD PRIMARY KEY (`id`),
@@ -280,78 +283,78 @@ ALTER TABLE `question`
   ADD KEY `idBonneReponse` (`idBonneReponse`);
 
 --
--- Indexes for table `reponse`
+-- Index pour la table `reponse`
 --
 ALTER TABLE `reponse`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idQuestion` (`idQuestion`);
 
 --
--- Indexes for table `technologie`
+-- Index pour la table `technologie`
 --
 ALTER TABLE `technologie`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `utilisateur`
+-- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `indice`
+-- AUTO_INCREMENT pour la table `indice`
 --
 ALTER TABLE `indice`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT for table `question`
+-- AUTO_INCREMENT pour la table `question`
 --
 ALTER TABLE `question`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
--- AUTO_INCREMENT for table `reponse`
+-- AUTO_INCREMENT pour la table `reponse`
 --
 ALTER TABLE `reponse`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
--- AUTO_INCREMENT for table `technologie`
+-- AUTO_INCREMENT pour la table `technologie`
 --
 ALTER TABLE `technologie`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `indice`
+-- Contraintes pour la table `indice`
 --
 ALTER TABLE `indice`
   ADD CONSTRAINT `techno` FOREIGN KEY (`idTechno`) REFERENCES `technologie` (`id`);
 
 --
--- Constraints for table `maitrise`
+-- Contraintes pour la table `maitrise`
 --
 ALTER TABLE `maitrise`
   ADD CONSTRAINT `maitrise2` FOREIGN KEY (`idTechnologie`) REFERENCES `technologie` (`id`),
   ADD CONSTRAINT `maitriser` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`email`);
 
 --
--- Constraints for table `question`
+-- Contraintes pour la table `question`
 --
 ALTER TABLE `question`
   ADD CONSTRAINT `question_ibfk_1` FOREIGN KEY (`idTechnologie`) REFERENCES `technologie` (`id`),
   ADD CONSTRAINT `question_ibfk_2` FOREIGN KEY (`idBonneReponse`) REFERENCES `reponse` (`id`);
 
 --
--- Constraints for table `reponse`
+-- Contraintes pour la table `reponse`
 --
 ALTER TABLE `reponse`
   ADD CONSTRAINT `reponse_ibfk_1` FOREIGN KEY (`idQuestion`) REFERENCES `question` (`id`);
